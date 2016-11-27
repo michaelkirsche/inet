53e9a6758f9a6d54d6d9906a0adc31ab0c2348d3
Michael Kirsche <kirscmic@tu-cottbus.de>
2016-11-27 16:46:23 +0100

    - Changed PHY structure back to IRadio module interface, so that IdealWireless and other radios can be used without problems - added files for 802154BPSKRadio, just a copy of IdealRadio for now

--- src/inet/linklayer/ieee802154Port/mac/Ieee802154MacBase.cc
+++ src/inet/linklayer/ieee802154Port/mac/Ieee802154MacBase.cc
@@ -23,15 +23,20 @@
 namespace inet {
 namespace ieee802154Port {
 
+simsignal_t Ieee802154MacBase::stateSignal = SIMSIGNAL_NULL;
+simsignal_t Ieee802154MacBase::radioStateSignal = SIMSIGNAL_NULL;
+
 Ieee802154MacBase::Ieee802154MacBase()
 {
-    // TODO Auto-generated constructor stub
+    // Auto-generated constructor stub
+    // TODO any initializations that need to be done here instead of Ieee802154MacBase::initialize ?
 
 }
 
 Ieee802154MacBase::~Ieee802154MacBase()
 {
-    // TODO Auto-generated destructor stub
+    // Auto-generated destructor stub
+    // TODO cancel timers. record statistics in Ieee802154MacBase::finish
 }
 
 void Ieee802154MacBase::initialize(int stage)
@@ -39,17 +44,36 @@ void Ieee802154MacBase::initialize(int stage)
     MACProtocolBase::initialize(stage);
 
     if (stage == INITSTAGE_LOCAL) {
-        EV << "Initializing stage 0\n";
+        EV_INFO_C("mac") << "Initializing stage 0\n";
 
-        // TODO initialize radio
+        // initialize radio module
+        cModule *radioModule = gate("lowerLayerOut")->getNextGate()->getOwnerModule();
+        radioModule->subscribe(IRadio::radioModeChangedSignal, this);
+        radioModule->subscribe(IRadio::receptionStateChangedSignal, this);
+        radioModule->subscribe(IRadio::transmissionStateChangedSignal, this);
+        radioModule->subscribe(IRadio::receivedSignalPartChangedSignal, this);
+        radio = check_and_cast<IRadio *>(radioModule);
 
-        // TODO initialize MAC address
+        // initialize MAC address
+        const char *addressString = par("macAddr");
+        if (!strcmp(addressString, "auto")) {
+            // change module parameter from "auto" to concrete address
+            par("macAddr").setStringValue(MACAddress::generateAutoAddress().str().c_str());
+            addressString = par("macAddr");
+        }
+        address.setAddress(addressString);
 
     }
     else if (stage == INITSTAGE_LINK_LAYER) {
-        EV << "Initializing stage 4\n";
+        EV_INFO_C("mac") << "Initializing stage 4\n";
 
         // TODO register interfaces
+//        registerInterface();
+
+        if (isOperational)
+            radio->setRadioMode(IRadio::RADIO_MODE_RECEIVER);
+//        if (isInterfaceRegistered().isUnspecified()) //TODO do we need multi-MAC feature? if so, should they share interfaceEntry??  --Andras
+//            registerInterface();
     }
 }
 
@@ -65,8 +89,8 @@ InterfaceEntry *Ieee802154MacBase::createInterfaceEntry()
 
     InterfaceEntry *e = new InterfaceEntry(this);
 
-//    // address
-//    e->setMACAddress(address);
+    // address
+    e->setMACAddress(address);
 //    e->setInterfaceToken(address.formInterfaceIdentifier());
 //
 //    e->setMtu(par("mtu").longValue());
@@ -79,31 +103,30 @@ InterfaceEntry *Ieee802154MacBase::createInterfaceEntry()
     return e;
 }
 
-
 void Ieee802154MacBase::handleSelfMessage(cMessage *msg)
 {
-    EV_DEBUG << "nothing done here in handleSelfMessage yet \n";
+    EV_DEBUG_C("mac") << "nothing done here in handleSelfMessage yet \n";
     ASSERT(false);
 }
 
 void Ieee802154MacBase::handleUpperPacket(cPacket *msg)
 {
-    EV_DEBUG << "nothing done here in handleUpperPacket yet \n";
+    EV_DEBUG_C("mac") << "nothing done here in handleUpperPacket yet \n";
 }
 
 void Ieee802154MacBase::handleLowerPacket(cPacket *msg)
 {
-    EV_DEBUG << "nothing done here in handleLowerPacket yet \n";
+    EV_DEBUG_C("mac") << "nothing done here in handleLowerPacket yet \n";
 }
 
 void Ieee802154MacBase::handleUpperCommand(cMessage *msg)
 {
-    EV_DEBUG << "nothing done here in handleUpperCommand yet \n";
+    EV_DEBUG_C("mac") << "nothing done here in handleUpperCommand yet \n";
 }
 
-// FIXME
 bool Ieee802154MacBase::handleNodeStart(IDoneCallback *doneCallback)
 {
+    // FIXME taken from 802.11 without adjustments
     if (!doneCallback)
         return true;    // do nothing when called from initialize()
 
@@ -112,17 +135,54 @@ bool Ieee802154MacBase::handleNodeStart(IDoneCallback *doneCallback)
     return ret;
 }
 
-// FIXME
 bool Ieee802154MacBase::handleNodeShutdown(IDoneCallback *doneCallback)
 {
+    // FIXME taken from 802.11 without adjustments
     bool ret = MACProtocolBase::handleNodeStart(doneCallback);
     handleNodeCrash();
     return ret;
 }
 
-// FIXME
 void Ieee802154MacBase::handleNodeCrash()
 {
+    // FIXME taken from 802.11 without adjustments
+}
+
+void Ieee802154MacBase::receiveSignal(cComponent *source, simsignal_t signalID, long value, cObject *details)
+{
+    // FIXME taken from 802.11 without adjustments
+    Enter_Method_Silent("receiveSignal()");
+//    if (signalID == IRadio::receptionStateChangedSignal) {
+//        //rx->receptionStateChanged((IRadio::ReceptionState)value);
+//    }
+//    else if (signalID == IRadio::transmissionStateChangedSignal) {
+//        auto oldTransmissionState = transmissionState;
+//        transmissionState = (IRadio::TransmissionState)value;
+//
+//        bool transmissionFinished = (oldTransmissionState == IRadio::TRANSMISSION_STATE_TRANSMITTING && transmissionState == IRadio::TRANSMISSION_STATE_IDLE);
+//
+//        if (transmissionFinished) {
+//            tx->radioTransmissionFinished();
+//            EV_DEBUG << "changing radio to receiver mode\n";
+//            configureRadioMode(IRadio::RADIO_MODE_RECEIVER);    //FIXME this is in a very wrong place!!! should be done explicitly from UpperMac!
+//        }
+//        rx->transmissionStateChanged(transmissionState);
+//    }
+//    else if (signalID == IRadio::receivedSignalPartChangedSignal) {
+//        rx->receivedSignalPartChanged((IRadioSignal::SignalPart)value);
+//    }
+}
+
+void Ieee802154MacBase::configureRadioMode(IRadio::RadioMode radioMode)
+{
+    // FIXME taken from 802.11 without adjustments
+    if (radio->getRadioMode() != radioMode) {
+        ConfigureRadioCommand *configureCommand = new ConfigureRadioCommand();
+        configureCommand->setRadioMode(radioMode);
+        cMessage *message = new cMessage("configureRadioMode", RADIO_C_CONFIGURE);
+        message->setControlInfo(configureCommand);
+        sendDown(message);
+    }
 }
 
 } /* namespace ieee802154Port */
